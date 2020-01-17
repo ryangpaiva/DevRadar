0a86a6449ab65c7cf4aaffa71ed8fe556b1b9f92
ryangpaiva <57689401+ryangpaiva@users.noreply.github.com>
2020-01-16 21:40:02 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-mcojw.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
+
+app.use(cors())
+app.use(express.json())
+app.use(routes);
+
+//get, post, put, delete
+
+//tipos de parametro:
+//Query Params: req.query (Filtros,ordenação, paginação,...) GET
+//Route Params: request.params (Identificar um recurso na alteração ou remoção) DELETE ou PUT
+//Body: request.body (Dados para criação ou alteração de um registro)
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//MongoDB (Não-Relacional)
 
+
+
+app.listen(3333);
